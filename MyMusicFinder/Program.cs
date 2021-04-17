9ff711218b42845411a24a4b5a0481d924f35976
martinklinden <72434888+martinklinden@users.noreply.github.com>
2021-04-18 00:01:48 +0200

    First commit, contains nothing

---
+++ MyMusicFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMusicFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code will be added, hopefully!");
+        }
+    }
+}
